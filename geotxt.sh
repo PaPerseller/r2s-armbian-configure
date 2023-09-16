6e8faf523bb29bb5d56786cd71ec196983a938e0
PaPerseller <huseraph@gmail.com>
2023-09-16 16:12:47 +0800

    change resources

--- geotxt.sh
+++ geotxt.sh
@@ -3,20 +3,20 @@
 TMPDIR=$(mktemp -d) || exit 1
 google_status=$(curl -I -4 -m 3 -o /dev/null -s -w %{http_code} http://www.google.com/generate_204)
 [ "$google_status" -ne "204" ] && mirror="https://ghproxy.com/"
-# geoip_cn
+# geoip.txt
 echo -e "\e[1;32mDownloading "$mirror"https://github.com/Hackl0us/GeoIP2-CN/raw/release/CN-ip-cidr.txt\e[0m"
 curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geoip_cn.txt" ""$mirror"https://github.com/Hackl0us/GeoIP2-CN/raw/release/CN-ip-cidr.txt"
 [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
 
-# geosite_cn
-echo -e "\e[1;32mDownloading "$mirror"https://raw.githubusercontent.com/QiuSimons/openwrt-mos/master/dat/geosite_cn.txt\e[0m"
-curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geosite_cn.txt" ""$mirror"https://raw.githubusercontent.com/QiuSimons/openwrt-mos/master/dat/geosite_cn.txt"
+# geosite_cn.txt
+echo -e "\e[1;32mDownloading "$mirror"https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt\e[0m"
+curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geosite_cn.txt" ""$mirror"https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt"
 [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
 
-# geosite-no_cn
-echo -e "\e[1;32mDownloading "$mirror"https://raw.githubusercontent.com/QiuSimons/openwrt-mos/master/dat/geosite_no_cn.txt\e[0m"
-curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geosite_no_cn.txt" ""$mirror"https://raw.githubusercontent.com/QiuSimons/openwrt-mos/master/dat/geosite_no_cn.txt"
+# geosite-nocn.txt
+echo -e "\e[1;32mDownloading "$mirror"https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt\e[0m"
+curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geosite_no_cn.txt" ""$mirror"https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt"
 [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
 
-cp -f "$TMPDIR"/* /etc/mosdns/rule/
+cp -f "$TMPDIR"/* /etc/mosdns/rule
 rm -rf "$TMPDIR"
