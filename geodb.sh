4c46a86db7301f18c45c2a8ad86770834e9385fd
PaPerseller <huseraph@gmail.com>
2023-09-16 16:12:28 +0800

    update

--- geodb.sh
+++ geodb.sh
@@ -3,9 +3,9 @@
 TMPDIR=$(mktemp -d) || exit 1
 google_status=$(curl -I -4 -m 3 -o /dev/null -s -w %{http_code} http://www.google.com/generate_204)
 [ "$google_status" -ne "204" ] && mirror="https://ghproxy.com/"
-# geoip.dat
+# geoip-cn.db
 echo -e "\e[1;32mDownloading "$mirror"https://github.com/soffchen/sing-geoip/releases/latest/download/geoip-cn.db\e[0m"
-curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geoip-cn.db" ""$mirror"https://github.com/soffchen/sing-geoip/releases/latest/download/geoip-cn.db"
+curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geoip.db" ""$mirror"https://github.com/soffchen/sing-geoip/releases/latest/download/geoip-cn.db"
 [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
 
 # geosite.db
